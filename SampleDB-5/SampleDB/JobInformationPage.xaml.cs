fa8b29d055f38e6ec97bb6de6f0037032554a215
kiranaluru80 <31380802+kiranaluru80@users.noreply.github.com>
2017-12-04 07:28:11 -0500

    collapsing issue

---
+++ SampleDB-5/SampleDB/JobInformationPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SampleDB
+{
+    public partial class JobInformationPage : ContentPage
+    {
+        public JobInformationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
